206326fc30939b993b6d888af7aaddd4455389a3
Pitney <PJules602@gmail.com>
2019-10-13 12:42:25 -0400

    Readline Package added

--- index.js
+++ index.js
@@ -1,18 +1,20 @@
-console.log( "Welcome to Batman: Arkham Asylum!" );
-console.log( "What do you want to do?" );
-let option = readlineSync.question( "Options: look around, smack a prisoner, run away" );
+let readlineSync = require('readline-sync');
 
-switch(option) {
+console.log("Welcome to Batman: Arkham Asylum!");
+console.log("What do you want to do?");
+let option = readlineSync.question("Options: look around, smack a prisoner, run away");
+
+switch (option) {
   case "look around":
-    console.log( "You see a bunch of angry prisoners and the Joker!" );
+    console.log("You see a bunch of angry prisoners and the Joker!");
     break;
   case "smack a prisioner":
-    console.log( "Ouch! What did I ever do to you, man?" );
+    console.log("Ouch! What did I ever do to you, man?");
     break;
   case "run away":
-    console.log( "Batman: \"Bats can't run.\"" );
+    console.log("Batman: \"Bats can't run.\"");
     break;
-  default: 
-    console.log( "A prisoner punches you in the face." );
-    console.log( "Prisoner: \"Quit messin' around.\"" );
+  default:
+    console.log("A prisoner punches you in the face.");
+    console.log("Prisoner: \"Quit messin' around.\"");
 }
